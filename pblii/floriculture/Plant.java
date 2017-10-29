7418fe685415efddebe5e1639565084acec0ea03
Matheus Souza <matheus.kruzsouza@gmail.com>
2017-10-29 20:51:20 -0300

    Classe Planta

---
+++ pblii/floriculture/Plant.java
@@ -0,0 +1,17 @@
+package floriculture;
+import java.util;
+
+public class Plant
+{
+	protected String especification;
+	protected int code;
+	protected String type;
+	protected int day;
+	protected int dayPlantWatered;
+	protected Vase vase;
+
+
+
+	
+
+}
